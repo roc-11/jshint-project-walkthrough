28627c3569634c18e82166ca105ac9f7caf7d621
roc-11 <>
2023-12-27 11:08:16 +0000

    Make post request to API using JS fetch()

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ const API_KEY = 'hvkJfCR_KjnyvqRdzN0LQ6HALmA';
 const API_URL = 'https://ci-jshint.herokuapp.com/api';
 const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
 
-document.getElementById('status').addEventListener('click', event => getStatus(event));
+/* ============= Check Status Button ======================*/
+document.getElementById('status').addEventListener('click', e => getStatus(e)); // e = event
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -24,6 +25,59 @@ function displayStatus(data) {
     let heading = 'API Status Key';
     let results =  `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById('resultsModalTitle').innerHTML = heading;
+
+    document.getElementById('results-content').innerHTML = results;
+
+    resultsModal.show();
+}
+
+/* ============= Run Checks Button ======================*/
+document.getElementById('submit').addEventListener('click', e => postForm(e));
+
+async function postForm(e) {
+    // get the form data from the HTML form 
+    const form = new FormData(document.getElementById('checksform'));
+
+    /* test 
+    for(let e of form.entries()) {
+        console.log(e);
+    }
+    */
+
+    const response = await fetch(API_URL, {
+                        method: "POST",
+                        headers: {
+                                    "Authorization": API_KEY,
+                                 }, 
+                        body: form
+                        });
+    
+    const data = await response.json();
+
+    if (response.ok) {
+        // console.log(data);
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayErrors(data) {
+
+    let heading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
 
     document.getElementById('resultsModalTitle').innerHTML = heading;
 
